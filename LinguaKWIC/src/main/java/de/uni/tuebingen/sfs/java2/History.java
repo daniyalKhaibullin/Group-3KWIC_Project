342884634c8493993f7d6f7adb4d7e01840cca07
aidayg24 <aidarostamee221bb@gmail.com>
2024-07-20 00:40:34 +0200

    edit

--- LinguaKWIC/src/main/java/de/uni/tuebingen/sfs/java2/History.java
+++ LinguaKWIC/src/main/java/de/uni/tuebingen/sfs/java2/History.java
@@ -6,19 +6,18 @@ public class History {
     public int match;
     public double frequency;
 
-    public History(String searchTopic, String target, int match, double frequency) {
+    public History(String searchTopic, String target, int match) {
         this.searchTopic = searchTopic;
         this.target = target;
         this.match = match;
-        this.frequency = frequency;
     }
 
     public String toString() {
         String html = "<html>" +
                 "<head>" +
                 "<style>" +
-                "body {font-family: Arial, sans-serif; margin: 0; padding: 15px; background-color: #9999BD;}" +
-                ".container {max-width: 600px; margin: auto; background: #9999BD; padding: 15px; border-radius: 10px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); color: #9999BD;}" +
+                "body {font-family: Arial, sans-serif; margin: 0; padding: 15px; background-color: transparent;}" +
+                ".container {max-width: 600px; margin: auto; padding: 15px; border-radius: 10px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); color: #9999BD;}" +
                 "h1 {text-align: center; color: #fff; font-size: 20px;}" +
                 "p {font-size: 14px; line-height: 1.6; color: #fff;}" +
                 ".highlight {font-weight: bold; color: #000}" +
@@ -30,10 +29,10 @@ public class History {
                 "<p>You have searched for <span class='highlight'>" + searchTopic + "</span><br>" +
                 "in <span class='highlight'>" + target + "</span>.</p>" +
                 "<p>You got <span class='highlight'>" + match + "</span> matches <br>" +
-//                "and the frequency of this search was <span class='highlight'>" + frequency + "</span>.</p>" +
                 "</div>" +
                 "</body>" +
                 "</html>";
         return html;
     }
+
 }
